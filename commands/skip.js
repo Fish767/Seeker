ac0ce3ffdf0b7c14d868b14aea5c2bc7de5da8b8
Fish767 <52433402+Fish767@users.noreply.github.com>
2019-12-23 18:24:54 -0500

    Delete skip.js

--- commands/skip.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'skip',
-    description: 'Skips a song in the queue.',
-    execute(client, receivedMessage, ytdl, servers, arguments) {
-        var server = servers[receivedMessage.guild.id];
-        if(server.dispatcher) server.dispatcher.end();
-        receivedMessage.channel.send("Skipping the current song.")
-    },
-};
