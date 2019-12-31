516fdaa19563f7aa63beacfe5a16660136b3ad2d
Fish767 <52433402+Fish767@users.noreply.github.com>
2019-12-31 12:09:02 -0500

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -4,8 +4,8 @@ module.exports = {
     execute(client, receivedMessage, ytdl, servers, arguments) {
         if (receivedMessage.member.hasPermission("ADMINISTRATOR")) {
             var server = servers[receivedMessage.guild.id];
-            if(server.dispatcher) server.dispatcher.end();
             receivedMessage.channel.send("Skipping the current song.")
+            if(server.dispatcher) server.dispatcher.end();
     }else {
             receivedMessage.channel.send("Oof. You need more permissions " + receivedMessage.author.toString()+".")
           };
